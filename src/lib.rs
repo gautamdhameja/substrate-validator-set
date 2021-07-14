7c64d7fac35bad532fafab4378924918a97783fa
Gautam Dhameja <gautamdhameja@users.noreply.github.com>
2021-07-14 17:04:40 +0200

    Updated readme.

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@ pub mod pallet {
     /// Configure the pallet by specifying the parameters and types on which it depends.
     #[pallet::config]
     pub trait Config: frame_system::Config + pallet_session::Config {
-        /// Because this pallet emits events, it depends on the runtime's definition of an event.
+        /// The Event type.
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
 
         /// Origin for adding or removing a validator.
